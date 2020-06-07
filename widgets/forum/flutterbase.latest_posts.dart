a1604320677319dcdc52143a6f94416576782fa3
JaeHo Song <thruthesky@gmail.com>
2020-06-07 21:47:34 +0800

    ref

--- widgets/forum/flutterbase.latest_posts.dart
+++ widgets/forum/flutterbase.latest_posts.dart
@@ -32,17 +32,18 @@ class _FlutterbaseLatestPostsState extends State<FlutterbaseLatestPosts> {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        shrinkWrap: true,
-        physics: NeverScrollableScrollPhysics(),
-        itemCount: posts.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('${posts[index].title}'),
-            trailing: Icon(Icons.keyboard_arrow_right),
-            onTap: () {
-              open(widget.route, arguments: {'post': posts[index]});
-            },
-          );
-        });
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: posts.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text('${posts[index].title}'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            open(widget.route, arguments: {'post': posts[index]});
+          },
+        );
+      },
+    );
   }
 }
